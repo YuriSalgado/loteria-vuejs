5309db1eba9381fd6606e3f57c4ac8e4f4d04dd4
YuriSalgado <yuri_salgadinho@hotmail.com>
2018-10-18 21:34:47 -0300

    Listagem de hits + select de tipo de jogo

--- js/concurse.js
+++ js/concurse.js
@@ -3,27 +3,20 @@ var concurse = new Vue({
     data: {
         concurse: {
             concurse: Math.floor(Math.random() * 5000) + 1,
-            concurses: 1
+            concurses: 1,
+            type: "LOTOFACIL"
         }
     },
     methods: {
     	saveData: function(event) {
-    		event.preventDefault();
-
     		var url = 'https://luckybox-prod.herokuapp.com/luckybox/bet/toBet';
-    		var dataAddiction = {
-    			/*concurse: Math.floor(Math.random() * 5000) + 1,
-    			concurses: 8,*/
-    			type:"LOTOFACIL"};
-debugger;
-			for (var attrname in dataAddiction) { 
-				this.concurse[attrname] = dataAddiction[attrname]; 
-			}
 
         	axios.post(url, this.concurse)
             	.then(res => {
 				console.log(res);
-            })	
+            });
+
+            event.preventDefault();
     	},
     }
 });
--- js/init.js
+++ js/init.js
@@ -2,6 +2,7 @@
   $(function(){
 
     $('.sidenav').sidenav();
+    $('select').formSelect();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
